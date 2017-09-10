ff2706af43bd47f7d90c4b363a81d13011f65c7e
Angel S. Moreno <angelxmoreno@gmail.com>
2017-09-10 17:07:58 -0400

    added setLimit tests

--- spec/suite/Collection.spec.php
+++ spec/suite/Collection.spec.php
@@ -1,7 +1,50 @@
 <?php
 
+use Kahlan\Plugin\Double;
 use Phalcon\Paginator\Adapter\Collection as Paginator;
 
 describe(Paginator::class, function () {
 
+    given('default_config', function () {
+        return [
+            'limit' => 3,
+        ];
+    });
+
+    describe('->__construct()', function () {
+        context('when a limit is given', function () {
+            beforeEach(function () {
+                $this->limit = 13;
+                $this->config = $this->default_config;
+                $this->config['limit'] = $this->limit;
+            });
+
+            it('sets the limit on the instance', function () {
+                expect(Paginator::class)
+                    ->toReceive('setLimit')
+                    ->with($this->limit);
+
+                $paginator = new Paginator($this->config);
+
+                expect($paginator->getLimit())->toBe($this->limit);
+            });
+        });
+
+        context('when a limit is not given', function () {
+            beforeEach(function () {
+                $this->config = $this->default_config;
+                unset($this->config['limit']);
+            });
+
+            it('sets the limit to "DEFAULT_LIMIT" on the instance', function () {
+                expect(Paginator::class)
+                    ->toReceive('setLimit')
+                    ->with(Paginator::DEFAULT_LIMIT);
+
+                $paginator = new Paginator($this->config);
+
+                expect($paginator->getLimit())->toBe(Paginator::DEFAULT_LIMIT);
+            });
+        });
+    });
 });
--- src/Collection.php
+++ src/Collection.php
@@ -16,4 +16,33 @@ use Phalcon\Paginator\AdapterInterface as PaginatorInterface;
  */
 class Collection implements PaginatorInterface
 {
-}
+    const DEFAULT_LIMIT = 30;
+
+    /**
+    /**
+     * @var integer
+     */
+    protected $limit;
+     * Adapter constructor
+     *
+     * @param array $config
+     */
+    public function __construct($config)
+    {
+        $limit = isset($config['limit']) ? $config['limit'] : self::DEFAULT_LIMIT;
+        $this->setLimit($limit);
+    }    /**
+     * @return int
+     */
+    public function getLimit()
+    {
+        return $this->limit;
+    }
+
+    /**
+     * @param int $limit
+     */
+    public function setLimit($limit)
+    {
+        $this->limit = (int)$limit;
+    }}
